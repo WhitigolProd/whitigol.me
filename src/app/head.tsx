cdc7b5271908c42a9d7f3899264aa582b7392a28
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-02-02 10:27:12 -0500

    🔨 (Head) — Update Page Title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title>Whitigol</title>
+            <title>Whitigol | My Portfolio</title>
             <meta
                 content="width=device-width, initial-scale=1"
                 name="viewport"
