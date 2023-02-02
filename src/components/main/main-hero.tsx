720f5093cdb263c2269a217ad571dc4c31cf4536
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-02-01 22:24:06 -0500

    🐛 (Hero) — Fix Unescaped Errors

--- src/components/main/main-hero.tsx
+++ src/components/main/main-hero.tsx
@@ -6,10 +6,10 @@ export default function MainHero() {
         <div className="flex flex-col items-center justify-center h-full w-full bg-gradient-to-b from-black to-gray-900">
             <div className="flex flex-col items-center justify-center w-full h-1/2">
                 <h1 className="text-5xl font-bold text-white">
-                    Hi, I'm <span className="text-blue-500">Jared</span>
+                    Hi, {"I'm"} <span className="text-blue-500">Jared</span>
                 </h1>
                 <h2 className="text-2xl font-bold text-white">
-                    I'm a{' '}
+                    {"I'm"} a{' '}
                     <span className="text-blue-500">
                         Frontend Web Developer
                     </span>
